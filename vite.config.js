d0a996ed0310f1188a1f6ad8db269aae11a50801
Rania Abohatab <raniaabohatab@dhcp-130-58-174-253.swarthmore.edu>
2025-08-07 17:58:37 -0400

    Fix Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/swarthmore-dorm-explorer/',
+  base: '/Swarthmore-dorm-explorer/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
